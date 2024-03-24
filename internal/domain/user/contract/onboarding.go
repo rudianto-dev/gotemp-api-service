4346c6292bd8e0a11306321d710ae324482b9488
Rudianto Tiofan <rudianto.dev@gmail.com>
2024-03-24 08:56:35 +0700

    update onboarding validation

--- internal/domain/user/contract/onboarding.go
+++ internal/domain/user/contract/onboarding.go
@@ -10,13 +10,13 @@ type Merchant struct {
 	Name     string  `json:"name" validate:"required"`
 	Address  string  `json:"address" validate:"required"`
 	Category string  `json:"category" validate:"required"`
-	QRIS     []*QRIS `json:"qris" validate:"required"`
+	QRIS     []*QRIS `json:"qris" validate:"required,dive,required"`
 }
 
 type CIF struct {
 	ID          string      `json:"id" validate:"required"`
 	PhoneNumber string      `json:"phone_number" validate:"required"`
-	Merchant    []*Merchant `json:"merchant" validate:"required"`
+	Merchant    []*Merchant `json:"merchant" validate:"required,dive,required"`
 }
 
 type OnboardingRequest struct {
